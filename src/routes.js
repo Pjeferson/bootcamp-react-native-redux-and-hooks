3b6e1039df7ce1ca806d9aed5c1240d82768a9d5
Pjeferson <paulojeferson34@gmail.com>
2020-01-14 11:58:32 -0300

    defining a default background color

--- src/routes.js
+++ src/routes.js
@@ -15,6 +15,9 @@ export default Routes = createAppContainer(
     {
       defaultNavigationOptions: {
         header: Header,
+        cardStyle: {
+          backgroundColor: '#191920',
+        },
       },
     }
   )
