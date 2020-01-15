e4945b17f1666af2270bb1ea76f7e2315e2e291c
Pjeferson <paulojeferson34@gmail.com>
2020-01-15 17:11:57 -0300

    implementing "Main" page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { View } from 'react-native';
-
-// import { Container } from './styles';
+import {
+  Container,
+  Products,
+  Product,
+  Logo,
+  Title,
+  Price,
+  AddButton,
+  ProductAmountText,
+  ProductAmount,
+  AddButtonText,
+} from './styles';
+import api from '../../services/api';
 
 export default class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await api.get('/products');
+
+    this.setState({
+      products: response.data,
+    });
+  }
+
+  handleAddProduct = id => {
+    console.log(id);
+  };
+
   render() {
-    return <View />;
+    const { products } = this.state;
+    return (
+      <Container>
+        <Products
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={products}
+          keyExtractor={product => String(product.id)}
+          renderItem={({ item }) => (
+            <Product>
+              <Logo source={{ uri: item.image }} />
+              <Title>{item.title}</Title>
+              <Price>{item.price}</Price>
+              <AddButton onPress={() => this.handleAddProduct(item.id)}>
+                <ProductAmount>
+                  <Icon name="add-shopping-cart" color="#FFF" size={16} />
+                  <ProductAmountText>{3 || 0}</ProductAmountText>
+                </ProductAmount>
+                <AddButtonText>ADICIONAR</AddButtonText>
+              </AddButton>
+            </Product>
+          )}
+        />
+      </Container>
+    );
   }
 }
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components/native';
+import { darken } from 'polished';
+
+export const Container = styled.View`
+  background: #141419;
+`;
+
+export const Products = styled.FlatList``;
+
+export const Product = styled.View`
+  background: #fff;
+  padding: 10px;
+  margin: 15px;
+  border-radius: 4px;
+  width: 220px;
+`;
+
+export const Logo = styled.Image`
+  height: 200px;
+  width: 200px;
+  background: #eee;
+`;
+
+export const Title = styled.Text`
+  font-size: 16px;
+  color: #333;
+`;
+
+export const Price = styled.Text`
+  margin: 10px 0;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const AddButton = styled.TouchableOpacity`
+  background: #7159c1;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 4px;
+  margin-top: auto;
+`;
+
+export const ProductAmount = styled.View`
+  padding: 12px;
+  background: ${darken(0.03, '#7159c1')};
+  flex-direction: row;
+  align-items: center;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+`;
+
+export const ProductAmountText = styled.Text`
+  color: #fff;
+  margin-left: 5px;
+`;
+export const AddButtonText = styled.Text`
+  flex: 1;
+  color: #fff;
+  font-weight: bold;
+  text-align: center;
+`;
