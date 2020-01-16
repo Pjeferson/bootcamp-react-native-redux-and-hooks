cc0f1881c4298e0e3bfd48e7f3574435a81d957e
Pjeferson <paulojeferson34@gmail.com>
2020-01-16 15:28:03 -0300

    creating navigation service

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 
 import './src/config/ReactotronConfig';
+import navigationService from './src/services/navigation';
 
 import Routes from './src/routes';
 
@@ -9,7 +10,11 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <Routes />
-    </>
+      <Routes
+        ref={navigationRef => {
+          navigationService.setTopLevelNavigator(navigationRef);
+        }}
+      />
+    </Provider>
   );
 }
