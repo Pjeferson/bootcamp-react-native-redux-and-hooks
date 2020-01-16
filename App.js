02883c738923a04e503c9d1b135e7cf70c47af9f
Pjeferson <paulojeferson34@gmail.com>
2020-01-16 14:26:50 -0300

    adding reactotron

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 
+import './src/config/ReactotronConfig';
+
 import Routes from './src/routes';
 
 export default function App() {
